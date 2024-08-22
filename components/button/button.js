ef1aea88265ffe20397710dd2600d0bcdc7f0ec9
Charlie <cbronson@gmail.com>
2024-08-22 12:21:35 -0500

    updated button

--- components/button/button.js
+++ components/button/button.js
@@ -3,28 +3,10 @@ class XButton extends HTMLElement {
         super();
         const shadow = this.attachShadow({ mode: 'open' });
         shadow.innerHTML = `
-                :host {
-                    --btn-bg: var(--primary-color, #007BFF);
-                    --btn-color: var(--text-color, #FFF);
-                    --btn-bg-hover: var(--secondary-color, #0056b3);
-                }
-                
-                button {
-                    padding: 10px 20px;
-                    background-color: var(--primary-color);
-                    color: var(--text-color);
-                    border: none;
-                    border-radius: 5px;
-                    cursor: pointer;
-                    font-size: var(--font-size, 16px);
-                    transition: background-color 0.3s ease;
-                }
-
-                button:hover {
-                    background-color: var(--secondary-color);
-                }
-            </style>
-            <button><slot></slot></button>
+        <link rel="stylesheet" href="/styles/global.css">
+        <link rel="stylesheet" href="/components/button/button.css">
+        <button class="button"><slot></slot></button>
+    `;
     }
 }
 
