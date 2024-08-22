071b2353f318cfddea19ae86be717d6708959d1f
Charlie <cbronson@gmail.com>
2024-08-22 12:24:12 -0500

    fixed spinner css ref

---
+++ components/spinner/spinner.js
@@ -0,0 +1,15 @@
+class XSpinner extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.innerHTML = `
+            <style>
+                @import url('/styles/global.css');
+                @import url('/components/spinner/spinner.css');
+            </style>
+            <div class="spinner"></div>
+        `;
+    }
+}
+
+customElements.define('x-spinner', XSpinner);
