de6577e3bbb46b30f32043b3cac5f87c2645870f
Charles Schmidt <cbronson@gmail.com>
2024-09-04 10:07:53 -0500

    updated styles for alert component

--- components/alert/nui-alert.js
+++ components/alert/nui-alert.js
@@ -2,13 +2,25 @@ class NUIAlert extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.render();
+    }
+
+    render() {
+        // Create a link element for the external CSS
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = '/components/alert/nui-alert.css'; 
+
+        // Create the structure of the alert
         this.shadowRoot.innerHTML = `
-            <link rel="stylesheet" href="nui-alert.css">
             <div class="alert">
                 <span class="close-btn" id="closeAlertButton">&times;</span>
                 <div class="alert-text"><slot></slot></div>
             </div>
         `;
+
+        // Append the link element to the Shadow DOM
+        this.shadowRoot.appendChild(link);
     }
 
     connectedCallback() {
@@ -20,6 +32,7 @@ class NUIAlert extends HTMLElement {
 
 customElements.define('nui-alert', NUIAlert);
 
+// Make showAlert globally accessible
 window.showAlert = function(message, type = 'info') {
     const alert = document.createElement('nui-alert');
     alert.classList.add(type);
