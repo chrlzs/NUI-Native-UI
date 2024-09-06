fbf024ee36206a4226e9ab665df3789d21e6e9e2
Charles Schmidt <cbronson@gmail.com>
2024-09-06 08:41:25 -0500

    updated CSS to target shadow dom

--- components/breadcrumbs/nui-breadcrumbs.js
+++ components/breadcrumbs/nui-breadcrumbs.js
@@ -2,23 +2,23 @@ class NUIBreadcrumbs extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+    }
 
-        // Attach external CSS
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
         const link = document.createElement('link');
         link.rel = 'stylesheet';
-        link.href = '/components/breadcrumbs/nui-breadcrumbs.css'; // Adjust the path as needed
-        this.shadowRoot.appendChild(link);
-
-        // Attach the inner structure of the component
-        this.shadowRoot.innerHTML += `
+        link.href = '/components/breadcrumbs/nui-breadcrumbs.css';
+        this.shadowRoot.innerHTML = `
             <nav class="breadcrumbs">
-                <ul>
-                    <slot></slot>
-                </ul>
+                <slot></slot>
             </nav>
         `;
+        this.shadowRoot.appendChild(link);
     }
 }
 
-// Define the custom element
 customElements.define('nui-breadcrumbs', NUIBreadcrumbs);
